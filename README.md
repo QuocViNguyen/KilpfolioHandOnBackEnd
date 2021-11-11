d9e328fc42e15381e277da2db588d0454b1af86d
Quoc Nguyen <charlesivnguyen4@yahoo.com>
2021-11-11 11:01:19 -0600

    update README.md

--- README.md
+++ README.md
@@ -6,3 +6,5 @@
 3. Open a new terminal
 4. Run ``npm start``
 5. Success! Your server is running on ``http://localhost:4000/``
+
+- This server is connected to a MongoDB database. 
---
+++ data.js
@@ -0,0 +1,35 @@
+const data = 
+[
+	{
+		"date": "8, 2021",
+		"access": 210,
+		"company": "Hawaiian"
+	},
+	{
+		"date": "8, 2021",
+		"access": 74,
+		"company": "Peperoni"
+	},
+	{
+		"date": "1, 2021",
+		"access": 460,
+		"company": "Sausage"
+	},
+	{
+		"date": "7, 2021",
+		"access": 112,
+		"company": "Italian Cheese"
+	},
+	{
+		"date": "5, 2021",
+		"access": 487,
+		"company": "Meat Lover"
+	},
+	{
+		"date": "12, 2021",
+		"access": 83,
+		"company": "Gourmet"
+	}
+]
+
+module.exports = data;
